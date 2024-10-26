49fd84330262d2b34c8705f64ddd8a1321ef39ac
Camilo Henao <camilo.henaoalvarez@ticketmaster.com>
2024-10-26 12:08:15 -0500

    app code updated getting the envs

--- app/helloWorld.js
+++ app/helloWorld.js
@@ -2,9 +2,7 @@
 const getEnvironments = () => {
     return {
         AI_API_KEY: process.env.OPENAI_API_KEY,
-        GREET: "Hello World",
     };
 };
 
-console.log("This is the Open AI api key value:");
-console.log(getEnvironments().AI_API_KEY);
+// Continue codging!
